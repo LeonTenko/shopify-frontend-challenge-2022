49e2a2caeada4ffde4252f4b7bf011d70e68d354
Leon <radraptorleo@gmail.com>
2021-09-12 16:34:53 -0700

    Provide simple grid layout using Material-Ui
    - Items will fill half of the screen on larger devices.
    - Items will fill 100% of the screen on smaller devices.
    - Card images are 16:9 now.
    - Card image layout = column.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
 import NasaImage from './components/nasa_image/NasaImage';
 
+const useStyles = makeStyles((theme) => {
+  return (
+    {
+      root: {
+        flexGrow: 1,
+      },
+    }
+  );
+});
+
 function App() {
+  const classes = useStyles();
 
   const [nasaImages, setNasaImages] = React.useState([]);
 
@@ -12,8 +26,8 @@ function App() {
 
     // Refactor into a separate file later
     const url = new URL("https://api.nasa.gov/planetary/apod");
-    const urlParams = { 
-      api_key: "EYNdwCv2ngW0Sf5h9ZUVICjuKhYNGNho47VOoZO6" 
+    const urlParams = {
+      api_key: "EYNdwCv2ngW0Sf5h9ZUVICjuKhYNGNho47VOoZO6"
     };
     Object.keys(urlParams).forEach(key => url.searchParams.append(key, urlParams[key]));
 
@@ -29,13 +43,28 @@ function App() {
     return responseJSON;
   };
 
-  React.useEffect( () => {
+  React.useEffect(() => {
     getNasaImages();
   }, []);
 
   return (
-    <div className="App">
-      <NasaImage></NasaImage>
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={3}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+
+        <Grid item xs={12} s={12} m={6} lg={6} xlg={6}>
+          <NasaImage></NasaImage>
+        </Grid>
+        <Grid item xs={12} s={12} m={6} lg={6} xlg={6}>
+          <NasaImage></NasaImage>
+        </Grid>
+        
+      </Grid>
     </div>
   );
 }
--- src/components/nasa_image/NasaImage.js
+++ src/components/nasa_image/NasaImage.js
@@ -10,15 +10,15 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    minWidth: "100%",
   },
   media: {
-    height: 140,
+    height: 0,
+    paddingTop: '56.25%',
   },
 });
 
 const NasaImage = () => {
-
   const classes = useStyles();
 
   return (
