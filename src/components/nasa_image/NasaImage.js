c224374168456e422063684944832fafab00c1e3
Leon <radraptorleo@gmail.com>
2021-09-12 15:31:51 -0700

    Add Material-UI
    - Install Material-UI with npm install.
    - Provide initial styling to NasaImageComponent with Material-Ui.

--- src/components/nasa_image/NasaImage.js
+++ src/components/nasa_image/NasaImage.js
@@ -1,8 +1,51 @@
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
 const NasaImage = () => {
-  return(
-    <div>
-      <h1>Test</h1>
-    </div>
+
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://apod.nasa.gov/apod/image/2109/LastRingPortrait_Cassini_1080.jpg"
+          title="Nasa Image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Test Image
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ulla
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" variant="outlined" color="default" endIcon={<Icon>thumb_up</Icon>}>
+          Like
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
