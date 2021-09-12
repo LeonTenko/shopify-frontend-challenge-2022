463bfc911f7df4e7ac4d211393bfb144ef161326
Leon <radraptorleo@gmail.com>
2021-09-12 11:45:34 -0700

    Set up initial GET request for Nasa Image data
    - Pings api.nasa.gov/planetary/apod api.
    - Gets data with provided parameters (retrieves one image so far).

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 import NasaImage from './components/nasa_image/NasaImage';
 
 function App() {
+
+  const [nasaImages, setNasaImages] = React.useState([]);
+
+  const getNasaImages = async () => {
+
+    // Refactor into a separate file later
+    const url = new URL("https://api.nasa.gov/planetary/apod");
+    const urlParams = { 
+      api_key: "EYNdwCv2ngW0Sf5h9ZUVICjuKhYNGNho47VOoZO6" 
+    };
+    Object.keys(urlParams).forEach(key => url.searchParams.append(key, urlParams[key]));
+
+    const response = await fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'default'
+    });
+
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+
+    return responseJSON;
+  };
+
+  React.useEffect( () => {
+    getNasaImages();
+  }, []);
+
   return (
     <div className="App">
       <NasaImage></NasaImage>
